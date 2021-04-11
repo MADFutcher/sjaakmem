82cf10a5076230e1fe668b158a7fa1f87a434eeb
MADFutcher <mikalob247@gmail.com>
2021-04-11 17:21:26 +0200

    Adjusted app.js

--- app.js
+++ app.js
@@ -86,4 +86,12 @@ app.use('/', memoryRoutes);
 const AuthRoutes = require('./routes/auth/Auth');
 app.use('/', AuthRoutes);
 
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
