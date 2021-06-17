67296c031c5421094b861775f503077b1f911fbd
MADFutcher <mikalob247@gmail.com>
2021-06-17 17:53:51 +0200

    updated memoryCard to use q_auto when uploading

--- client/src/components/memory/memoryCard.js
+++ client/src/components/memory/memoryCard.js
@@ -4,11 +4,16 @@ import ReactPlayer from 'react-player'
 
 
 export default function MemoryCard(props) {
+    let video = '' 
+    if (props.image.includes('.mp')){
+        video = props.image.substr(0, 50) + 'q_auto' + props.image.substr(49)
+    }
+    
     if(props.image && props.memory){
         return (
             <Card className="text-center" style={{backgroundColor:`${props.cardColour}`,color:`${props.textColour}`,justifyContent: 'center'}}>
                 {props.image.includes('.mp') && 
-                    <ReactPlayer url={props.image} width='100%' height='100%' controls/>
+                    <ReactPlayer url={video} width='100%' height='100%' controls/>
                 }
                 {!props.image.includes('.mp') && 
                     <Card.Img variant="top" src={props.image} style={{objectFit:'contain', maxWidth:'100%'}} />
@@ -37,7 +42,7 @@ export default function MemoryCard(props) {
             <Card className="text-center" style={{backgroundColor:`${props.cardColour}`,color:`${props.textColour}`,justifyContent: 'center'}}>
                 
                  {props.image.includes('.mp') && 
-                    <ReactPlayer url={props.image} width='100%' height='100%' controls/>
+                    <ReactPlayer url={video} width='100%' height='100%' controls/>
                 }
                 {!props.image.includes('.mp') &&
                     <Card.Img variant='top' src={props.image} style={{objectFit:'contain', maxWidth:'100%'}}/>
@@ -52,7 +57,7 @@ export default function MemoryCard(props) {
             <Card className="text-center" style={{backgroundColor:`${props.cardColour}`,color:`${props.textColour}`,justifyContent: 'center'}}>
                 
                  {props.image.includes('.mp') && 
-                    <ReactPlayer url={props.image} width='100%' height='100%' controls/>
+                    <ReactPlayer url={video} width='100%' height='100%' controls/>
                 }
                 {!props.image.includes('.mp') &&
                     <Card.Img variant='top' src={props.image} style={{objectFit:'contain', maxWidth:'100%'}}/>
